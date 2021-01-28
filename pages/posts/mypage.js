507a053cdfea6f34ef91d5fd2dbe115923161698
jungle379 <jungle379@gmail.com>
2021-01-28 21:43:07 +0900

    changedate

--- pages/posts/mypage.js
+++ pages/posts/mypage.js
@@ -10,7 +10,7 @@ export default function FirstPost() {
     </Head>
       <h1>自己紹介</h1>
       <p>
-      主に東京へ旅行しています。高いところと遠くから全体を写す写真が多めですがお楽しみください！！
+      年１回は旅行に行くようにしています。遠くから全体を写す写真が多めですがご覧になってください！！
       </p>
       <h2>
         <Link href="/">
@@ -20,18 +20,3 @@ export default function FirstPost() {
     </>
   )
 }
-
-// export const getStaticProps = async () => {
-//   const key = {
-//     headers: {'X-API-KEY': process.env.API_KEY},
-//   };
-
-//   const res = await fetch('https://travelblog.microcms.io/api/v1/news',key);
-
-//   const data = await res.json();
-//   return {
-//     props: {
-//       data: data.contents,
-//     },
-//   };
-// };
