eb953114590378bf765de998e39607cf93ce200e
jungle379 <jungle379@gmail.com>
2021-02-17 17:32:56 +0900

    修正

--- pages/archive/[page].js
+++ pages/archive/[page].js
@@ -1,6 +1,6 @@
 import fs from "fs"
 import Link from "next/link"
-import Layout from "../../components/Layout"
+import Layout from "../../components/layout"
 import Pager from "../../components/Pager"
 import { getSortedPostsData, listContentFiles } from "../../lib/posts"
 
